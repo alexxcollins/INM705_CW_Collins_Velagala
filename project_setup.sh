8c56179ff45dd15a256658cc0881cc4f013ae71b
alexxcollins <alex.collins@googlemail.com>
2022-05-10 10:37:57 +0000

    changes to project_setup.sh

--- project_setup.sh
+++ project_setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "Installing dependencies"
+pip install ujson
+
 echo "Setting up folders"
 
 mkdir -p ./Datasets ./Datasets/coco ./Datasets/coco/images ./Datasets/coco/annotations ./Datasets/coco/images/train2017 ./Datasets/coco/images/val2017
@@ -15,7 +18,7 @@ then
     echo " - Training annotations already exist"
 else
     wget -P ./Datasets/coco/annotations https://s3-us-west-2.amazonaws.com/dl.fbaipublicfiles.com/LVIS/lvis_v1_train.json.zip
-    unzip ./Datasets/coco/annotations/lvis_v1_train.json.zip -d /Datasets/coco/annotations
+    unzip ./Datasets/coco/annotations/lvis_v1_train.json.zip -d ./Datasets/coco/annotations
     rm ./Datasets/coco/annotations/lvis_v1_train.json.zip
     echo " - Downloaded training annotations"
 fi
@@ -25,7 +28,7 @@ then
     echo " - Validation annotations already exist"
 else
     wget -P ./Datasets/coco/annotations https://s3-us-west-2.amazonaws.com/dl.fbaipublicfiles.com/LVIS/lvis_v1_val.json.zip
-    unzip ./Datasets/coco/annotations/lvis_v1_val.json.zip -d /Datasets/coco/annotations
+    unzip ./Datasets/coco/annotations/lvis_v1_val.json.zip -d ./Datasets/coco/annotations
     rm ./Datasets/coco/annotations/lvis_v1_val.json.zip
     echo " - Downloaded validation notations"
 fi
